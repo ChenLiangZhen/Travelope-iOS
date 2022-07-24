b146984538369247ef97b4472cb4222254df255e
Chen Liang Zhen <felix961308@gmail.com>
2022-07-24 11:33:38 +0800

    Initial Commit

---
+++ travelope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  travelope
+//
+//  Created by Lightiichen on 2022/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
